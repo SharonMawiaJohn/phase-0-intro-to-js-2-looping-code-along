8ad8dd8b6c329953c34e6133df1a1ed512c52b67
SharonMawiaJohn <sharonmawia76@gmail.com>
2024-06-29 13:20:55 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+}
+  
+module.exports = {
+    writeCards
+  };
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+  
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  
+countDown(10);
